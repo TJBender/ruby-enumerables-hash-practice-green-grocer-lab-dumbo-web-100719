5fc0d7fe0e32cf148aaa216ce13b0352c28d46b8
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 03:55:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,7 +8,7 @@ def consolidate_cart(cart)
       if final_hash.has_key?(element_name)
         element_stats[:count] += 1
       else 
-        final_hash[element_name] = element_stats
+        final_hash[element_name] = {}
       end
   end
   final_hash
