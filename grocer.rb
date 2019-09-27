fb565879818a2bd1d48cb80ef19f52e4cdf82c74
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 04:25:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,7 +24,7 @@ def apply_cupons(cart, coupons)
         cart["#{item} W/COUPON"] = {
           price: coupon[:cost] / coupon[:num],
           clearance: cart[:item][:clearance],
-          count: coupon[:num],
+          count: coupon[:num]
         }
     end
   end
