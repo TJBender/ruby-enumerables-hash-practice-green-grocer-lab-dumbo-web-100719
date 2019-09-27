6843458b71e4d1721e702ee36aa9a0150921485e
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 03:43:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,15 @@
+require 'pry'
 def consolidate_cart(cart)
-  # code here
-end
-
-def apply_coupons(cart, coupons)
-  # code here
-end
-
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
-end
+  final_hash = {}
+  cart.each do |element_hash|
+    element_name = element_hash.keys[0]
+    element_stats = element_hash.values[0]
+    
+      if final_hash.has_key?(element_name)
+        element_stats[:count] += 1
+      else 
+        
+        binding.pry
+      end
+  end
+end 
