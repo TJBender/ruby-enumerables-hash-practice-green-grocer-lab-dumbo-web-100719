03959b32a1f1971f1d98f6d2eaa521338cf35c37
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 05:05:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,5 +48,5 @@ end
 def checkout(array, coupons) 
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
-  applied_discount = 
+  applied_discount = apply_coupons
 end
