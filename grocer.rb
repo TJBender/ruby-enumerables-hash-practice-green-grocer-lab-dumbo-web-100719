0080a4a208dca13d2fc7791df36c4c5a7e56a27c
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 05:13:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,5 +50,5 @@ def checkout(array, coupons)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
   total = applied_discount.reduce(0) { |acc, (key, val)| acc += val[:price] * val[:count]}
-  total > 100? total * 0.10
+  total > 100? total * 0.9
 end
