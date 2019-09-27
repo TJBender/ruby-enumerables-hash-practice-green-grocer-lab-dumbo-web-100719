1c0e7f351b1450b070e081a1a0e4af96d311c79b
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 04:42:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,7 +29,7 @@ def apply_cupons(cart, coupons)
           count: coupon[:num]
         }
         cart[item][:count] -= coupon[:num]
-      elsif (cart[item][:count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON"))
+        elsif (cart[item][:count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON"))
         
         cart["#{item} W/COUPON"][:count] += coupon[:num]
         cart[item][:count] -= coupon[:num]
